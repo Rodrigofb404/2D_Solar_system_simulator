4f9a6afd976e2003eccde9cd34e2aaa426909bc5
Miguel Mota <94927896+miguelmota2301@users.noreply.github.com>
2025-05-23 20:58:33 -0300

    Add Vector2D class
    Vector2D has all basic vector operations and some override methods.

--- src/main/java/org/ufba/Main.java
+++ src/main/java/org/ufba/Main.java
@@ -1,7 +1,13 @@
 package org.ufba;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Vector2D v = new Vector2D(4,5);
+        Vector2D v2 = new Vector2D(3,2);
+
+        double mag = v.magnitude();
+        double d = v.euclideanDistance(v2);
+        System.out.println("Magnitude of v: " + mag + "\nEuclidean distance between v and v2: " + d);
     }
 }
---
+++ src/main/java/org/ufba/Vector2D.java
@@ -0,0 +1,81 @@
+package org.ufba;
+
+public class Vector2D {
+    private double x;
+    private double y;
+
+    public Vector2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getters
+    public double getX() {return x;}
+    public double getY() {return y;}
+
+    // Setters
+    public void setX(double x) {this.x = x;}
+    public void setY(double y) {this.y = y;}
+
+    // Vector Operations
+    // Addition
+    public Vector2D add(Vector2D v) {
+        return new Vector2D(x + v.getX(), y + v.getY());
+    }
+
+    // Subtraction
+    public Vector2D sub(Vector2D v) {
+        return new Vector2D(x - v.getX(), y - v.getY());
+    }
+
+    // Scalar multiplication
+    public Vector2D multiply(double scalar) {
+        return new Vector2D(x * scalar, y * scalar);
+    }
+
+    // Scalar division
+    public Vector2D divide(double scalar) {
+        if (scalar == 0) throw new ArithmeticException("Division by zero");
+        return new Vector2D(x / scalar, y / scalar);
+    }
+
+    // Dot Product
+    public Vector2D dotProduct(Vector2D v) {
+        return new Vector2D(x * v.getX(), y * v.getY());
+    }
+
+    // Magnitude
+    public double magnitude() {
+        return Math.sqrt(x*x + y*y);
+    }
+
+    // Normalization
+    public Vector2D normalize() {
+        if (x == 0 && y == 0) return this;
+        return new Vector2D(x / magnitude(), y / magnitude());
+    }
+
+    // Euclidean Distance
+    public double euclideanDistance(Vector2D v) {
+        return sub(v).magnitude();
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (!(obj instanceof Vector2D)) return false;
+
+        Vector2D v = (Vector2D) obj;
+        return x == v.getX() && y == v.getY();
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(x, y);
+    }
+}
