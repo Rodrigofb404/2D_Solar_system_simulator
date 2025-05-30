4754f579eb9f3ad29370c0fdf93c3faab915d7a2
Rodrigo Freitas <rodrigof0501@gmail.com>
2025-05-30 17:00:02 -0300

    Moved the GUI code to the GUI file

--- src/main/java/org/ufba/Main.java
+++ src/main/java/org/ufba/Main.java
@@ -4,44 +4,10 @@ import java.awt.Color;
 import javax.swing.*;
 
 public class Main {
-
-    static public void createGUI() {
-        // Create the frame, the super class
-        JFrame frame = new JFrame("Teste");
-        frame.setSize(1500, 800);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        // Create two different panels 
-        JPanel galaxy = new JPanel();
-        galaxy.setBackground(Color.BLACK);
-        galaxy.add(new JLabel("Planets will be here"));
-        
-        JPanel controls = new JPanel();
-        controls.setBackground(Color.GRAY);
-        controls.add(new JLabel("controls will be here"));
-        
-        frame.add(galaxy);
-        frame.add(controls);
-
-        // Calculate the divider location
-        int frameWidth = frame.getWidth();
-        int galaxyWidth = (int) (0.85 * frameWidth);
-        
-        // Create a component that will be used to split the window 
-        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, galaxy, controls);
-
-        // Configure the divider
-        splitPane.setDividerSize(8);
-        splitPane.setDividerLocation(galaxyWidth);
-        splitPane.setResizeWeight(0.85); // When resized, 85% of the space go to galaxy and 15% to controls
-        
-        splitPane.setOneTouchExpandable(true);
-
-        frame.add(splitPane);
-        
-        frame.setVisible(true);
-    }   
     public static void main(String[] args) {
-        createGUI();
+        // Sempre rode GUIs no Event Dispatch Thread
+        SwingUtilities.invokeLater(() -> {
+            GUI.createGUI(); // agora vai chamar corretamente
+        });
     }
 }
