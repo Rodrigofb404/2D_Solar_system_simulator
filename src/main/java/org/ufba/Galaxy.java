d930c6ba101eaf804fa81193929513886d407a2e
Rodrigo Freitas <rodrigof0501@gmail.com>
2025-06-15 15:02:04 -0300

    Refactor Galaxy.java

--- src/main/java/org/ufba/Galaxy.java
+++ src/main/java/org/ufba/Galaxy.java
@@ -10,7 +10,7 @@ public class Galaxy extends JPanel {
     private Star sun;
 
     // Constants
-    static private final int sunRadius = 35;
+    static private final int sunRadius = 50;
     private final int galaxyCenterX = (int) (GUI.getGalaxyCenter().getX());
     private final int galaxyCenterY = (int) (GUI.getGalaxyCenter().getY());
 
@@ -21,6 +21,31 @@ public class Galaxy extends JPanel {
         setBackground(Color.BLACK);
     }
 
+    private void drawPlanetarySystem(Graphics g) {
+        for (Planet body : planetarySystem.getPlanets()) {
+            Vector2D pos = body.getPosition();
+
+            // Get distance from sun and decrease the size of the orbits
+            double distReal = pos.magnitude();
+            double distVisual = Math.sqrt(distReal); 
+            
+            // Create the position vector of each planet
+            Vector2D direction = pos.normalize();
+            Vector2D posVisual = direction.multiply(distVisual);
+
+            final double SCALE = 2.5e-4; // size factor 
+
+            // Adjust orbits' sizes to fit on the frame
+            int x = (int) (posVisual.getX() * SCALE);
+            int y = (int) (posVisual.getY() * SCALE);
+    
+            g.setColor(body.getColor());
+    
+            g.fillOval(x - 20, y - 20, 10, 10);
+            }
+        }
+
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -30,29 +55,7 @@ public class Galaxy extends JPanel {
 
         g.fillOval(-sunRadius, -sunRadius, sunRadius, sunRadius);
 
-        for (Planet body : planetarySystem.getPlanets()) {
-            Vector2D pos = body.getPosition();
-            Vector2D relativePos = pos.sub(sun.getPosition());
+        drawPlanetarySystem(g);
 
-            double distReal = relativePos.magnitude();
-            double distVisual = Math.sqrt(distReal);
-
-            Vector2D direction = relativePos.normalize();
-            Vector2D posVisual = direction.multiply(distVisual);
-
-            double scale = 2.5e-4; // Adjust to fit on the frame
-
-            int x = (int) (posVisual.getX() * scale);
-            int y = (int) (posVisual.getY() * scale);
-    
-            if (body instanceof Planet) {
-                g.setColor(body.getColor());
-            } else {
-                g.setColor(Color.WHITE);
-            }
-    
-                g.fillOval(x - 5, y - 5, 8, 8);
-            }
-        }
-        
     }
+}
