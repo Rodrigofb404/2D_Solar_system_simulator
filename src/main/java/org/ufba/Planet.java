dc44a7115d0f1ad041a797a45d6aabfa61d54c55
Rodrigo Freitas <rodrigof0501@gmail.com>
2025-07-09 12:38:38 -0300

    Refactor: Delete useless atribute

--- src/main/java/org/ufba/Planet.java
+++ src/main/java/org/ufba/Planet.java
@@ -3,22 +3,15 @@ package org.ufba;
 import java.awt.Color;
 
 public class Planet extends CelestialBody {
-    private int orbitalPeriod;
-
     private Color color;
 
-    public Planet(String name, int orbitalPeriod, double mass, Vector2D position, Vector2D velocity, Color color) {
+    public Planet(String name, double mass, Vector2D position, Vector2D velocity, Color color) {
         super(name, mass, position, velocity);
         this.name = name;
-        this.orbitalPeriod = orbitalPeriod;
         this.color = color;
         }   
 
         public Color getColor() {
             return color;
-        }
-        
-        public int getOrbitalPeriod () {
-            return orbitalPeriod;
-        }
+        }        
 }
